bbe69c9e8cff69073d11f698a7216cc696171d0f
Julia Nedialkova <julianedialkova@hotmail.com>
2019-05-09 14:24:16 +0300

    Build with -mod vendor
    Co-authored-by: Georgi Sabev <georgethebeatle@gmail.com>

--- ci/build-binaries.sh
+++ ci/build-binaries.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-export GOPATH=$PWD
+BINARY_PATH="${PWD}/plugin-binaries/cpu-entitlement-plugin-$(cat version/number)"
 
-PLUGIN_PATH=$GOPATH/src/code.cloudfoundry.org/cpu-entitlement-plugin
-BINARY_PATH="${PWD}/plugin-binaries/$(basename $PLUGIN_PATH)-$(cat version/number)"
-
-cd $PLUGIN_PATH
-GOOS=linux   GOARCH=amd64 go build -o ${BINARY_PATH}.linux64
-GOOS=linux   GOARCH=386   go build -o ${BINARY_PATH}.linux32
-GOOS=windows GOARCH=amd64 go build -o ${BINARY_PATH}.win64
-GOOS=windows GOARCH=386   go build -o ${BINARY_PATH}.win32
-GOOS=darwin  GOARCH=amd64 go build -o ${BINARY_PATH}.osx
+cd cpu-entitlement-plugin
+GOOS=linux   GOARCH=amd64 go build -mod vendor -o ${BINARY_PATH}.linux64
+GOOS=linux   GOARCH=386   go build -mod vendor -o ${BINARY_PATH}.linux32
+GOOS=windows GOARCH=amd64 go build -mod vendor -o ${BINARY_PATH}.win64
+GOOS=windows GOARCH=386   go build -mod vendor -o ${BINARY_PATH}.win32
+GOOS=darwin  GOARCH=amd64 go build -mod vendor -o ${BINARY_PATH}.osx
