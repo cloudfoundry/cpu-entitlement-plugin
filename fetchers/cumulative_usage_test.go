9525e315769fad3e7dfdfc8cc009b3a5a08f40fd
Kieron Browne <kbrowne@pivotal.io>
2019-11-06 12:17:39 +0000

    backfill cumulative fetcher integration tests
    and remove redundant unit tests
    
    [#167669898]
    
    Co-authored-by: Giuseppe Capizzi <gcapizzi@pivotal.io>

--- fetchers/cumulative_usage_test.go
+++ fetchers/cumulative_usage_test.go
@@ -1,9 +1,7 @@
 package fetchers_test
 
 import (
-	"context"
 	"errors"
-	"fmt"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -29,84 +27,14 @@ var _ = Describe("Fetchers/CumulativeUsage", func() {
 
 		appGuid = "foo"
 
-		logCacheClient.PromQLReturns(queryResult(
-			sample("0", "abc",
-				point("1", 0.2),
-			),
-			sample("1", "def",
-				point("2", 0.4),
-			),
-			sample("2", "ghi",
-				point("4", 0.5),
-			),
-		), nil)
 		appInstances = map[int]cf.Instance{
 			0: cf.Instance{InstanceID: 0, ProcessInstanceID: "abc"},
-			1: cf.Instance{InstanceID: 1, ProcessInstanceID: "def"},
-			2: cf.Instance{InstanceID: 2, ProcessInstanceID: "ghi"},
 		}
 
 	})
 
 	JustBeforeEach(func() {
 		cumulativeUsage, fetchErr = fetcher.FetchInstanceData(appGuid, appInstances)
-	})
-
-	It("gets the current usage from the log-cache client", func() {
-		Expect(logCacheClient.PromQLCallCount()).To(Equal(1))
-		ctx, query, _ := logCacheClient.PromQLArgsForCall(0)
-		Expect(ctx).To(Equal(context.Background()))
-		Expect(query).To(Equal(fmt.Sprintf(`absolute_usage{source_id="%s"} / absolute_entitlement{source_id="%s"}`, appGuid, appGuid)))
-	})
-
-	It("returns the correct accumulated usage", func() {
-		Expect(fetchErr).NotTo(HaveOccurred())
-		Expect(cumulativeUsage).To(SatisfyAll(HaveLen(3), HaveKey(0), HaveKey(1), HaveKey(2)))
-		Expect(cumulativeUsage[0].(fetchers.CumulativeInstanceData).Usage).To(Equal(0.2))
-		Expect(cumulativeUsage[1].(fetchers.CumulativeInstanceData).Usage).To(Equal(0.4))
-		Expect(cumulativeUsage[2].(fetchers.CumulativeInstanceData).Usage).To(Equal(0.5))
-	})
-
-	When("cache returns data for instances that are no longer running (because the app has been scaled down", func() {
-		BeforeEach(func() {
-			appInstances = map[int]cf.Instance{
-				0: cf.Instance{InstanceID: 0, ProcessInstanceID: "abc"},
-			}
-		})
-
-		It("returns current usage for running instances only", func() {
-			Expect(fetchErr).NotTo(HaveOccurred())
-			Expect(cumulativeUsage).To(SatisfyAll(HaveLen(1), HaveKey(0)))
-			Expect(cumulativeUsage[0].(fetchers.CumulativeInstanceData).Usage).To(Equal(0.2))
-		})
-	})
-
-	When("cache returns data for instances with same id but different process instance id", func() {
-		BeforeEach(func() {
-			logCacheClient.PromQLReturns(queryResult(
-				sample("0", "def",
-					point("1", 0.2),
-				),
-			), nil)
-		})
-
-		It("ignores that data", func() {
-			Expect(cumulativeUsage).To(BeEmpty())
-		})
-	})
-
-	When("cache returns data for instances with unknown process instance id", func() {
-		BeforeEach(func() {
-			logCacheClient.PromQLReturns(queryResult(
-				sample("0", "xyz",
-					point("1", 0.5),
-				),
-			), nil)
-		})
-
-		It("ignores that data", func() {
-			Expect(cumulativeUsage).To(BeEmpty())
-		})
 	})
 
 	When("fetched data has corrupt instance id", func() {
--- integration/cumulative_usage_fetcher_test.go
+++ integration/cumulative_usage_fetcher_test.go
@@ -40,58 +40,11 @@ var _ = Describe("Cumulative Usage Fetcher", func() {
 
 	When("running multiple apps with various instance counts", func() {
 		BeforeEach(func() {
-			app1inst1 := emitters.GaugeMetric{
-				SourceId:   app1ID,
-				InstanceId: "1",
-				Tags: map[string]string{
-					"process_instance_id": "1",
-				},
-				Values: []emitters.GaugeValue{
-					{Name: "absolute_usage", Value: 134, Unit: "nanoseconds"},
-					{Name: "absolute_entitlement", Value: 186, Unit: "nanoseconds"},
-				},
-			}
-
-			app1inst2 := emitters.GaugeMetric{
-				SourceId:   app1ID,
-				InstanceId: "2",
-				Tags: map[string]string{
-					"process_instance_id": "2",
-				},
-				Values: []emitters.GaugeValue{
-					{Name: "absolute_usage", Value: 135, Unit: "nanoseconds"},
-					{Name: "absolute_entitlement", Value: 137, Unit: "nanoseconds"},
-				},
-			}
-
-			app1inst3 := emitters.GaugeMetric{
-				SourceId:   app1ID,
-				InstanceId: "3",
-				Tags: map[string]string{
-					"process_instance_id": "3",
-				},
-				Values: []emitters.GaugeValue{
-					{Name: "absolute_usage", Value: 136, Unit: "nanoseconds"},
-					{Name: "absolute_entitlement", Value: 138, Unit: "nanoseconds"},
-				},
-			}
-
-			app2inst1 := emitters.GaugeMetric{
-				SourceId:   app2ID,
-				InstanceId: "1",
-				Tags: map[string]string{
-					"process_instance_id": "10",
-				},
-				Values: []emitters.GaugeValue{
-					{Name: "absolute_usage", Value: 236, Unit: "nanoseconds"},
-					{Name: "absolute_entitlement", Value: 238, Unit: "nanoseconds"},
-				},
-			}
-
-			emitGauge(app1inst1)
-			emitGauge(app1inst2)
-			emitGauge(app1inst3)
-			emitGauge(app2inst1)
+			emitUsage(app1ID, "1", "1", 100, 150)
+			emitUsage(app1ID, "1", "1", 134, 186)
+			emitUsage(app1ID, "2", "2", 135, 137)
+			emitUsage(app1ID, "3", "3", 136, 138)
+			emitUsage(app2ID, "1", "10", 236, 238)
 		})
 
 		It("gets the usages of all instances for each app", func() {
@@ -103,13 +56,39 @@ var _ = Describe("Cumulative Usage Fetcher", func() {
 			instances2 := map[int]cf.Instance{
 				1: {InstanceID: 1, ProcessInstanceID: "10"},
 			}
-			var app1Usages map[int]interface{}
-			Eventually(func() map[int]interface{} { app1Usages = getUsages(app1ID, instances1); return app1Usages }).Should(HaveLen(3))
+			Eventually(func() map[int]interface{} { return getUsages(app1ID, instances1) }).Should(HaveLen(3))
 			Eventually(func() map[int]interface{} { return getUsages(app2ID, instances2) }).Should(HaveLen(1))
+		})
+
+		It("gets the latest value when > 1 exists", func() {
+			instances1 := map[int]cf.Instance{
+				1: {InstanceID: 1, ProcessInstanceID: "1"},
+			}
+			var app1Usages map[int]interface{}
+			Eventually(func() map[int]interface{} { app1Usages = getUsages(app1ID, instances1); return app1Usages }).Should(HaveLen(1))
 
 			app1Inst1Usage, ok := app1Usages[1].(fetchers.CumulativeInstanceData)
 			Expect(ok).To(BeTrue(), "couldn't cast fetcher result to CumulativeInstanceData")
-			Expect(app1Inst1Usage.Usage).To(BeNumerically("~", 0.72043, 0.00001))
+			Expect(app1Inst1Usage.Usage).To(BeNumerically("~", 134.0/186.0, 0.00001))
+		})
+	})
+
+	When("a metric exists for an old instance", func() {
+		BeforeEach(func() {
+			emitUsage(app1ID, "1", "1", 134, 186)
+			emitUsage(app1ID, "1", "old", 50, 100)
+		})
+
+		It("is ignored, and an earlier correct instance value is returned", func() {
+			instances1 := map[int]cf.Instance{
+				1: {InstanceID: 1, ProcessInstanceID: "1"},
+			}
+			var app1Usages map[int]interface{}
+			Eventually(func() map[int]interface{} { app1Usages = getUsages(app1ID, instances1); return app1Usages }).Should(HaveLen(1))
+
+			app1Inst1Usage, ok := app1Usages[1].(fetchers.CumulativeInstanceData)
+			Expect(ok).To(BeTrue(), "couldn't cast fetcher result to CumulativeInstanceData")
+			Expect(app1Inst1Usage.Usage).To(BeNumerically("~", 134.0/186.0, 0.00001))
 		})
 	})
 
@@ -132,4 +111,16 @@ var _ = Describe("Cumulative Usage Fetcher", func() {
 
 func getCmdOutput(cmd string, args ...string) string {
 	return strings.TrimSpace(string(Cmd(cmd, args...).Run().Out.Contents()))
+}
+
+func emitUsage(appID, instanceID, processInstanceID string, usage, entitlement float64) {
+	emitGauge(emitters.GaugeMetric{
+		SourceId:   appID,
+		InstanceId: instanceID,
+		Tags:       map[string]string{"process_instance_id": processInstanceID},
+		Values: []emitters.GaugeValue{
+			{Name: "absolute_usage", Value: usage, Unit: "nanoseconds"},
+			{Name: "absolute_entitlement", Value: entitlement, Unit: "nanoseconds"},
+		},
+	})
 }
